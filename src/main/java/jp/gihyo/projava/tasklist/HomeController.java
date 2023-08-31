7fc8ccbc584fb437ffa76cffc0c54ad36a1534f9
kinkaku-hideki <hideki20230525@gmailcom>
2023-08-31 12:03:31 +0900

    HomeController.javaを追加 TaskListDao.javaを追加

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -1,5 +1,6 @@
 package jp.gihyo.projava.tasklist;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,20 +14,45 @@ import java.util.UUID;
 
 @Controller
 public class HomeController {
-    record TaskItem(String id, String task, String deadline, boolean done) { }
+    record  TaskItem(String id, String task, String deadline, boolean done) {}
     private List<TaskItem> taskItems = new ArrayList<>();
-
-    @GetMapping("/list")
-    String listItems(Model model) {
-        model.addAttribute("tasklist", taskItems);
-        return "home";
+    private final TaskListDao dao;
+    @Autowired
+    HomeController(TaskListDao dao) {
+        this.dao = dao;
+    }
+    @RequestMapping(value="/hello")
+    String hello(Model model) {
+        model.addAttribute("time",LocalDateTime.now());
+        return "hello";
     }
     @GetMapping("/add")
     String addItem(@RequestParam("task")String task,
-                   @RequestParam("deadline") String deadline){
+                   @RequestParam("deadline") String deadline) {
         String id = UUID.randomUUID().toString().substring(0,8);
         TaskItem item = new TaskItem(id, task, deadline, false);
-        taskItems.add(item);
+        dao.add(item);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/list")
+    String listItems(Model model) {
+        List<TaskItem> taskItems = dao.findAll();
+        model.addAttribute("tasklist", taskItems);
+        return "home";
+    }
+    @GetMapping("/delete")
+    String deleteItem(@RequestParam("id") String id) {
+        dao.delete(id);
+        return "redirect:/list";
+    }
+    @GetMapping("/update")
+    String updateItem(@RequestParam("id") String id,
+                      @RequestParam("task") String task,
+                      @RequestParam("deadline") String deadline,
+                      @RequestParam("done") boolean done) {
+        TaskItem taskItem = new TaskItem(id, task, deadline, done);
+        dao.update(taskItem);
         return "redirect:/list";
     }
 }
